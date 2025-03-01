091b2d3f804c8afbd98b5330e9cc1308ae84878c
NanoClem <clemdecoop.pro@gmail.com>
2025-03-02 00:42:01 +0100

    feat : initialize dagster project

---
+++ dagster-essentials/setup.py
@@ -0,0 +1,12 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="dagster_essentials",
+    packages=find_packages(exclude=["dagster_essentials_tests"]),
+    install_requires=[
+        "dagster",
+        "dagster-cloud",
+        "pandas>=2.2.3",
+    ],
+    extras_require={"dev": ["dagster-webserver", "pytest"]},
+)
